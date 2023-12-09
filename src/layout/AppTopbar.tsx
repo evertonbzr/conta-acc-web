0d4cb9a130ec525b3ce75832daba88add8c2899c
Everton Fernandes <evertonfrnds@gmail.com>
2023-12-09 16:54:40 -0300

    Add Avatar component to AppTopbar

--- src/layout/AppTopbar.tsx
+++ src/layout/AppTopbar.tsx
@@ -12,6 +12,7 @@ import AppContainer from './AppContainer';
 import { Menu } from 'primereact/menu';
 import { menu } from './constants';
 import { ItemMenu } from '../types/layout';
+import { Avatar } from 'primereact/avatar';
 
 const MenuLink = ({ item }: { item: ItemMenu }) => {
     const menuLeft = useRef<Menu>(null);
@@ -30,11 +31,25 @@ const MenuLink = ({ item }: { item: ItemMenu }) => {
             <Menu
                 ref={menuLeft}
                 popup
+                className="bg-gray-900"
                 model={item.items?.map((el) => ({
-                    label: el.label,
-                    icon: el.icon,
-                    command: () => {
-                        router.push(el.to as string);
+                    template: (item, options) => {
+                        return (
+                            <li>
+                                <Link href={el.to as string} className="p-ripple flex p-3 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 transition-colors transition-duration-150 cursor-pointer">
+                                    <i className={classNames('mr-2', el.icon)}></i>
+                                    <span className="font-medium">{el.label}</span>
+                                    <span
+                                        role="presentation"
+                                        className="p-ink"
+                                        style={{
+                                            height: '0px',
+                                            width: '0px'
+                                        }}
+                                    ></span>
+                                </Link>
+                            </li>
+                        );
                     }
                 }))}
             />
@@ -74,62 +89,17 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
 
                             return (
                                 <li>
-                                    <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150">
+                                    <Link
+                                        href={item.to as string}
+                                        className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150"
+                                    >
                                         <i className="pi pi-home mr-2"></i>
                                         <span>{item.label}</span>
                                         <span className="p-ink" style={{ height: '99.9802px', width: '99.9802px' }}></span>
-                                    </a>
+                                    </Link>
                                 </li>
                             );
                         })}
-                        {/* <li>
-                            <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150">
-                                <i className="pi pi-home mr-2"></i>
-                                <span>Home</span>
-                                <span className="p-ink" style={{ height: '99.9802px', width: '99.9802px' }}></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                onClick={(event) => menuLeft.current!.toggle(event)}
-                                className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150"
-                            >
-                                <i className="pi pi-calendar mr-2"></i>
-                                <span>Customers</span>
-                                <span className="p-ink" style={{ height: '120.208px', width: '120.208px' }}></span>
-                            </a>
-                            <Menu
-                                ref={menuLeft}
-                                popup
-                                model={[
-                                    {
-                                        label: 'Update',
-                                        icon: 'pi pi-refresh',
-
-                                        command: () => {}
-                                    },
-                                    {
-                                        label: 'Delete',
-                                        icon: 'pi pi-times',
-                                        command: () => {}
-                                    }
-                                ]}
-                            />
-                        </li>
-                        <li>
-                            <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150">
-                                <i className="pi pi-calendar mr-2"></i>
-                                <span>Calendar</span>
-                                <span className="p-ink" style={{ height: '120.208px', width: '120.208px' }}></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150">
-                                <i className="pi pi-chart-line mr-2"></i>
-                                <span>Stats</span>
-                                <span className="p-ink" style={{ height: '91.0545px', width: '91.0545px' }}></span>
-                            </a>
-                        </li> */}
                     </ul>
                     <ul className="list-none p-0 m-0 flex lg:align-items-center select-none flex-column lg:flex-row border-top-1 border-gray-800 lg:border-top-none">{/* ... rest of the code ... */}</ul>
                 </div>

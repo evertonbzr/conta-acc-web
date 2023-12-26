0fc5fba3e0b8b3679c27ecfd2c703c53f5cd5b2b
Everton Fernandes <evertonfrnds@gmail.com>
2023-12-26 13:34:43 -0300

    Update API URL and remove unused code in AppMenu

--- src/layout/AppMenu.tsx
+++ src/layout/AppMenu.tsx
@@ -6,24 +6,23 @@ import { LayoutContext } from './context/layoutcontext';
 import { MenuProvider } from './context/menucontext';
 import Link from 'next/link';
 import { AppMenuItem } from '../types/types';
-import { model } from './constants';
 
 const AppMenu = () => {
-    const { layoutConfig } = useContext(LayoutContext);
+  const { layoutConfig } = useContext(LayoutContext);
 
-    return (
-        <MenuProvider>
-            <ul className="layout-menu">
-                {model.map((item, i) => {
-                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
-                })}
-
-                <Link href="https://blocks.primereact.org" target="_blank" style={{ cursor: 'pointer' }}>
-                    <img alt="Prime Blocks" className="w-full mt-3" src={`/layout/images/banner-primeblocks${layoutConfig.colorScheme === 'light' ? '' : '-dark'}.png`} />
-                </Link>
-            </ul>
-        </MenuProvider>
-    );
+  return (
+    <MenuProvider>
+      <ul className="layout-menu">
+        <Link href="https://blocks.primereact.org" target="_blank" style={{ cursor: 'pointer' }}>
+          <img
+            alt="Prime Blocks"
+            className="w-full mt-3"
+            src={`/layout/images/banner-primeblocks${layoutConfig.colorScheme === 'light' ? '' : '-dark'}.png`}
+          />
+        </Link>
+      </ul>
+    </MenuProvider>
+  );
 };
 
 export default AppMenu;
